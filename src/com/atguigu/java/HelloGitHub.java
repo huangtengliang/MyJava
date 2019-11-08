0c5fcfa39a55e3168ca80494094d2b72e8b92623
黄藤亮 <805414989@qq.com>
2019-11-08 21:19:05 +0800

    update by github

--- src/com/atguigu/java/HelloGitHub.java
+++ src/com/atguigu/java/HelloGitHub.java
@@ -3,8 +3,10 @@ package com.atguigu.java;
 public class HelloGitHub {
 	public static void main(String[] args) {
 		
-		System.out.println("Hello GitHub!");
+		System.out.println("eclipse : Hello GitHub!");
 		
+		
+		System.out.println("GitHub : Hello GitHub!");
 	}
 
 }
